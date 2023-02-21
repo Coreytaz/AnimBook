6ee10c00cadc3dbcb2fbc9535ab58fb3865c6b09
Coreytaz <coreytaz321@gmail.com>
2023-02-21 17:56:35 +0500

    add ImageSlider | add product

--- _pages/ProductPage/product/index.tsx
+++ _pages/ProductPage/product/index.tsx
@@ -1,5 +1,6 @@
-import { FC } from 'react'
-import { Card, Grid, Text } from '@nextui-org/react'
+import { FC, useEffect, useRef, useState } from 'react'
+import { Button, Card, Grid, Spacer, Text } from '@nextui-org/react'
+import ImageSlider from '@/features/image-slider/ui'
 
 const Product: FC = () => {
     return (
@@ -11,7 +12,7 @@ const Product: FC = () => {
                 }}
                 weight="bold"
             >
-                Товар
+                Случайная фигурка Genshin Impact
             </Text>
             <ProductCard />
         </>
@@ -19,11 +20,56 @@ const Product: FC = () => {
 }
 
 const ProductCard: FC = () => {
+    const dateImages = [
+        {
+            src: 'https://nextui.org/images/card-example-1.jpeg',
+            alt: 'Card image background',
+        },
+        {
+            src: 'https://nextui.org/images/card-example-2.jpeg',
+            alt: 'Card image background',
+        },
+        {
+            src: 'https://nextui.org/images/card-example-3.jpeg',
+            alt: 'Card image background',
+        },
+        {
+            src: 'https://nextui.org/images/card-example-4.jpeg',
+            alt: 'Card image background',
+        },
+        {
+            src: 'https://nextui.org/images/card-example-5.jpeg',
+            alt: 'Card image background',
+        },
+        {
+            src: 'https://nextui.org/images/card-example-6.jpeg',
+            alt: 'Card image background',
+        },
+    ]
     return (
         <Card>
-            <Grid.Container gap={2} justify="center">
-                <Grid xs={6}></Grid>
-                <Grid xs={6}></Grid>
+            <Grid.Container gap={2} justify="center" css={{ p: '$11' }}>
+                <Grid xs={5} css={{ flexDirection: 'column', gap: '$10' }}>
+                    <ImageSlider imageCollection={dateImages} />
+                </Grid>
+                <Grid xs={7} css={{ flexDirection: 'column', justifyContent: 'space-around' }}>
+                    <div>
+                        <Text b size="$3xl" span>
+                            890.00 руб
+                        </Text>
+                    </div>
+                    <div>
+                        <Text>
+                            Случайная фигурка по игре Genshin Impact ! Вам может попасться один из
+                            начальных персонажей , которые были с путешественником с самого начала
+                            твоего пути в игре.
+                        </Text>
+                    </div>
+                    <div>
+                        <Button>Купить</Button>
+                    </div>
+                    <Spacer />
+                </Grid>
             </Grid.Container>
         </Card>
     )
---
+++ features/image-slider/ui/index.tsx
@@ -0,0 +1,72 @@
+import { Card } from '@nextui-org/react'
+import { FC, useEffect, useRef, useState } from 'react'
+import styles from './styles.module.scss'
+
+interface ImageSliderProps {
+    imageCollection: {
+        src: string
+        alt: string
+    }[]
+}
+
+const ImageSlider: FC<ImageSliderProps> = ({ imageCollection }) => {
+    const scrollVerticalRef = useRef<HTMLDivElement>(null!)
+    const [selectImage, setSelectImage] = useState(0)
+
+    const onChangePreviewImage = (index: number) => {
+        setSelectImage(index)
+    }
+
+    const onWheel = (e: WheelEvent) => {
+        e.preventDefault()
+        scrollVerticalRef.current.scrollTo({
+            left: +scrollVerticalRef.current.scrollLeft + e.deltaY * 4,
+            behavior: 'smooth',
+        })
+    }
+
+    useEffect(() => {
+        if (scrollVerticalRef.current) {
+            scrollVerticalRef.current.addEventListener('wheel', onWheel)
+            return () => {
+                // eslint-disable-next-line react-hooks/exhaustive-deps
+                scrollVerticalRef.current.removeEventListener('wheel', onWheel)
+            }
+        }
+    }, [])
+    return (
+        <>
+            <Card>
+                <Card.Image
+                    src={imageCollection[selectImage].src}
+                    objectFit="cover"
+                    showSkeleton
+                    width="100%"
+                    height={450}
+                    alt={imageCollection[selectImage].alt}
+                />
+            </Card>
+            <div className={styles.imageCollection} ref={scrollVerticalRef}>
+                {imageCollection.map((image, i) => (
+                    <Card
+                        css={{ minWidth: 95, minH: 100, cursor: 'pointer' }}
+                        key={image.src}
+                        isHoverable
+                    >
+                        <Card.Image
+                            onClick={() => onChangePreviewImage(i)}
+                            src={image.src}
+                            objectFit="cover"
+                            showSkeleton
+                            width="100%"
+                            height={100}
+                            alt={image.alt}
+                        />
+                    </Card>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default ImageSlider
