c6194dfa734300368e041aa46c29a46686b8e798
Coreytaz <coreytaz321@gmail.com>
2023-03-21 17:05:17 +0500

    add page Cart

---
+++ _pages/CartPage/Sidebar/index.tsx
@@ -0,0 +1,31 @@
+import { useOrder } from '@/entities/cart'
+import { toRub } from '@/shared'
+import { Button, Card, Text } from '@nextui-org/react'
+import React from 'react'
+
+const Sidebar = () => {
+    const { price } = useOrder()
+    return (
+        <Card css={{ mw: '400px' }}>
+            <Card.Body
+                css={{
+                    display: 'flex',
+                    flexFlow: 'nowrap',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                }}
+            >
+                <Text css={{ display: 'block' }}>Итого</Text>
+                <Text b css={{ display: 'block' }}>
+                    {toRub.format(price)}
+                </Text>
+            </Card.Body>
+            <Card.Divider />
+            <Card.Footer css={{ justifyContent: 'center' }}>
+                <Button>Перейти к оформлению</Button>
+            </Card.Footer>
+        </Card>
+    )
+}
+
+export default Sidebar
--- _pages/CartPage/index.tsx
+++ _pages/CartPage/index.tsx
@@ -1,9 +1,23 @@
 'use client'
-import { Container } from '@nextui-org/react'
+import { useActionOrder } from '@/entities/cart'
+import { Col, Container, Row } from '@nextui-org/react'
 import { FC } from 'react'
+import Content from './Content'
+import Sidebar from './Sidebar'
 
 const CartPage: FC = () => {
-    return <Container lg>CartPage</Container>
+    return (
+        <Container lg>
+            <Row gap={1}>
+                <Col>
+                    <Content />
+                </Col>
+                <Col span={4}>
+                    <Sidebar />
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 
 export default CartPage
