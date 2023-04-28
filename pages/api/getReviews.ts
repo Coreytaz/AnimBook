fdfb7539db871a4fbcc669b9cd2486e7b3353e76
Coreytaz <coreytaz321@gmail.com>
2023-04-28 17:50:08 +0500

    fix eslint

--- pages/api/getReviews.ts
+++ pages/api/getReviews.ts
@@ -2,10 +2,7 @@ import { ProductDescription, ReviewsProps } from '@/shared/api'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { Reviews } from './data/reviews'
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<ReviewsProps[]>
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ReviewsProps[]>) {
     if (req.method === 'GET') {
         const productSlug = req.query.slug
         const reviews = Reviews.filter((item) => item.productSlug === productSlug)
