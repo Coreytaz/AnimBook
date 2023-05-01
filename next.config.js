6369e7eda83fdd62bcd68e59d383ec83687b2a44
Coreytaz <coreytaz321@gmail.com>
2023-05-01 12:40:41 +0500

    fix

--- next.config.js
+++ next.config.js
@@ -8,6 +8,16 @@ const nextConfig = {
     env: {
         API_URL: 'http://localhost:3000/api',
     },
+    async headers() {
+        return [
+            {
+                source: '/_next/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Origin', value: 'https://anim-book.vercel.app' },
+                ],
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
