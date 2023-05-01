1290a4f73d769c48beae0ab789303f13fda1cc56
Coreytaz <coreytaz321@gmail.com>
2023-05-01 13:05:03 +0500

    fix

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
         return [
             {
                 // matching all API routes
-                source: '/_next/:path*',
+                source: '/api/:path*',
                 headers: [
                     { key: 'Access-Control-Allow-Credentials', value: 'true' },
                     { key: 'Access-Control-Allow-Origin', value: '*' },
