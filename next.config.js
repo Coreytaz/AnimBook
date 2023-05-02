b05bcc9168d04734dacbf612fe70deb5c95e8eaf
Coreytaz <coreytaz321@gmail.com>
2023-05-02 13:14:44 +0500

    fix

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
         appDir: true,
     },
     env: {
-        API_URL: 'http://localhost:3000/api',
+        API_URL: process.env.API_URL,
     },
     async headers() {
         return [
