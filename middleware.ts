9be71b2895643ee5237f25513c630e64ae76aee9
Coreytaz <coreytaz321@gmail.com>
2023-06-01 14:14:23 +0500

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-// export { default } from 'next-auth/middleware'
+export { default } from 'next-auth/middleware'
 // import { withAuth } from 'next-auth/middleware'
 // import { NextResponse } from 'next/server'
 
